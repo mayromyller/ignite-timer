fc4dcd1463063e5ea7958f5e9d47655f9834bc36
Mayro <deehmyller11@gmail.com>
2022-08-04 10:15:10 -0300

    refatorar: separando as funções do reducer do Context

---
+++ src/reducers/cycles/recuder.ts
@@ -0,0 +1,55 @@
+import { ActionType } from './action'
+
+export interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionType.CREATE_NEW_CYCLE: {
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id
+      }
+    }
+    case ActionType.INTERRUPT_CYCLE: {
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null
+      }
+    }
+    case ActionType.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null
+      }
+    default: {
+      return state
+    }
+  }
+}

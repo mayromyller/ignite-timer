6be598686fb864f57852d60ac50c12f979627f60
Mayro <deehmyller11@gmail.com>
2022-08-04 00:48:37 -0300

    refatorar: adiciona CycleContext para mapear todos os ciclos

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,15 @@
+import { useContext } from 'react'
+
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { CyclesContext } from '../../contexts/CycleContext'
+
 import * as S from './style'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <S.Container>
       <S.Title>Meu histórico</S.Title>
@@ -16,14 +25,31 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>há 2 dias</td>
-              <td>
-                <S.Status statusType="green">Concluído</S.Status>
-              </td>
-            </tr>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <S.Status statusType="green">Concluído</S.Status>
+                    )}
+                    {cycle.interruptedDate && (
+                      <S.Status statusType="red">Interrompido</S.Status>
+                    )}
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <S.Status statusType="yellow">Em andamento</S.Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </S.List>
